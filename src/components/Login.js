857a9f5a24eff26fc5625f9a81f1f0ef72d54134
Shaun Colley <swc7838@gmail.com>
2019-06-17 21:21:42 -0500

    now storing data in local state on Login.js, need to move to server side to begin registration and login process

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,30 @@ import { NavLink } from 'react-router-dom'
 import '../css/Login.css'
 
 class Login extends Component {
+  constructor(){
+    super()
+    this.state = {
+      username: "",
+      password: "",
+    }
+  }
+
+  handleTextBoxChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleLoginClick = () => {
+    console.log(this.state)
+  }
+
   render() {
     return(
       <div className="login-body">
-        <input type='text' placeholder="username"/>
-        <input type='password' placeholder="password" />
-        <button className="login-btn">Login</button>
+        <input type='text' onChange={this.handleTextBoxChange} placeholder="username" name="username"/>
+        <input type='password' onChange={this.handleTextBoxChange} placeholder="password" name="password"/>
+        <button className="login-btn" onClick={this.handleLoginClick}>Login</button>
         <NavLink to='/register'>Not a member? Register now to save your garden plans.</NavLink>
       </div>
     )
