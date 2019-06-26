dbbe4e0a39b0085230ef3e630a8286b4dc35b2ff
Shaun Colley <swc7838@gmail.com>
2019-06-26 12:35:55 -0500

    profile displays now, need to add ability to update and style. Then customizing and adding plants

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: false,
+    };
+  };
 
   componentDidMount(){
     let url = 'http://localhost:8080/api' + this.props.match.url;
     axios.get(url)
     .then(response => {
-      console.log(response.data);
+      const { email, id, firstName, lastName, favVeg, zipCode } = response.data
+      this.setState({ user: true, email, id, firstName, lastName, favVeg, zipCode });
     });
   };
 
+  handleUpdateProfileClick = (id) => {
+    console.log(`update profile ${id}`);
+  }
+
   render() {
-    return <h3>Profile</h3>;
+    if(this.state.user){
+      const { email, id, firstName, lastName, favVeg, zipCode } = this.state;
+      return <Fragment><h3>Profile</h3>
+      <ul>
+      <li>Name: {firstName} {lastName}</li>
+      <li>Email: {email}</li>
+      <li> Favorite Vegetable: {favVeg}</li>
+      <li>Zip Code: {zipCode}</li>
+      </ul>
+      <button onClick={() => this.handleUpdateProfileClick(id)}></button>
+      </Fragment>;
+    }
+    return <Fragment><h3>Profile</h3>
+    </Fragment>;
   }
 }
 
