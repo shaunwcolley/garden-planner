52b315135b7872dc6b04982b2a6873f5f5826b07
Shaun Colley <swc7838@gmail.com>
2019-07-15 09:08:53 -0500

    styling changes

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -41,45 +41,55 @@ class Calendar extends Component {
     if(plans) {
       plans.forEach(plan => {
         plan.cells.forEach(cell => {
-          const {id, plant: { name, firstHarvestDate }, updatedAt} = cell
-          let plantedDate = new Date(updatedAt);
-          const harvestDate = addDays(plantedDate,parseInt(firstHarvestDate));
-          const row = (
-                      <tr key={id}>
-                        <td>{plan.name}</td>
-                        <td>{name}</td>
-                        <td><Moment format="MMMM DD YYYY">{plantedDate}</Moment></td>
-                        <td><Moment format="MMMM DD YYYY">{harvestDate}</Moment></td>
-                      </tr>
-                    );
-          display.push(row);
+          if(cell.plant){
+            const {id, plant: { name, firstHarvestDate }, updatedAt} = cell
+            let plantedDate = new Date(updatedAt);
+            const harvestDate = addDays(plantedDate,parseInt(firstHarvestDate));
+            const row = (
+                        <tr key={id}>
+                          <td>{plan.name}</td>
+                          <td>{name}</td>
+                          <td><Moment format="MMMM DD YYYY">{plantedDate}</Moment></td>
+                          <td><Moment format="MMMM DD YYYY">{harvestDate}</Moment></td>
+                        </tr>
+                      );
+            display.push(row);
+          }
         })
       })
     }
 
-    return <div>
-            <h3 className="calendar-heading">
+    return <div className="calendar-container">
+            <h2 className="calendar-heading">
               Calendar
-            </h3>
-            <table className="calendar">
-              <tbody>
-                <tr>
-                  <th>
-                    Plan
-                  </th>
-                  <th>
-                    Plant
-                  </th>
-                  <th>
-                    Planted
-                  </th>
-                  <th>
-                    Earliest Expected Harvest
-                  </th>
-                </tr>
-                {display}
-              </tbody>
-            </table>
+            </h2>
+            <div className="calendar">
+              <table>
+                <thead>
+                  <tr>
+                    <th className="sticky-header">
+                      Plan
+                    </th>
+                    <th className="sticky-header">
+                      Plant
+                    </th>
+                    <th className="sticky-header">
+                      Planted
+                    </th>
+                    <th className="sticky-header">
+                      Earliest Expected Harvest
+                    </th>
+                  </tr>
+                  <tr className="sticky-border">
+                    <th>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {display}
+                </tbody>
+              </table>
+            </div>
            </div>
   }
 }
