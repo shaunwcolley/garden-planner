def59ba58b8ab0a468a86623e73e3ab58420ee53
Shaun Colley <swc7838@gmail.com>
2019-07-20 12:54:12 -0500

    styling the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import "../css/About.css";
 
 class About extends Component {
   render(){
     return (
-      <div className="home-body">
-        <h4>Howdy, this is a garden planner app. Follow links above to register as a new user or to login if you are an existing user.</h4>
+      <div className="about-container">
+        <div className="about-body">
+          <div className="about-circle top-circle">
+            <span>Gardening</span>
+          </div>
+          <div className="about-circle middle-circle">
+            <span>is</span>
+          </div>
+          <div className="about-circle bottom-circle">
+            <span>fun!</span>
+          </div>
+        </div>
+        <div className="about-footer">
+          <img className="flower-left flower" alt="carnation" src="https://cdn.pixabay.com/photo/2012/04/18/21/01/carnation-37872_960_720.png"/>
+          <img alt="grass" src="https://cdn.pixabay.com/photo/2014/03/24/17/17/flowers-295321_960_720.png"/>
+          <img className="flower-right flower" alt="flower" src="https://cdn.pixabay.com/photo/2017/01/31/21/37/flower-2027457_960_720.png"/>
+        </div>
       </div>
     )
   }
